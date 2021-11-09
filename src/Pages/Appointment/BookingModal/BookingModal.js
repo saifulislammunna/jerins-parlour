21dccf59df5b66e7b7911fc8fc97248c6503cf21
saifulislammunna <saifulmunna28@gmail.com>
2021-11-09 10:22:58 +0600

    added

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -4,6 +4,17 @@ import { Button, Form, Modal  } from 'react-bootstrap';
 const BookingModal = ({show,handleClose,booking}) => {
    
 const  {name} = booking;
+
+const handleBookingSubmit = e =>  {
+     alert('submiting');
+    //  collect data from the form
+
+    // send data to the server
+ 
+
+     handleClose();
+     e.preventDefault();
+}
   return (
     <>
   
@@ -11,10 +22,15 @@ const  {name} = booking;
       <Modal show={show} onHide={handleClose}>
          
         <Modal.Body> 
-        <Form>
+        <Form onSubmit={ handleBookingSubmit }>
   <Form.Group className="mb-3" controlId="formBasicText">
     
-    <Form.Control type="text" placeholder="" />
+    <Form.Control disabled type="text" placeholder= {name}/>
+    
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicText">
+    
+    <Form.Control type="text" placeholder="Your Name" />
      
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -22,14 +38,9 @@ const  {name} = booking;
     <Form.Control type="email" placeholder="Enter email" />
      
   </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
+  <Form.Group className="mb-3" controlId="formBasicNumber">
     
-    <Form.Control type="email" placeholder="Enter email" />
-     
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="number" placeholder="Phone Number" />
      
   </Form.Group>
 
